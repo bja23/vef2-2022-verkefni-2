b184dfdb770b3c4e9ef4b5a9f4e1722a9db9bff9
bja23 <42986015+bja23@users.noreply.github.com>
2022-02-11 20:11:59 +0000

    test

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,10 +1,21 @@
 INSERT INTO events (name,slug,created,updated )
-VALUES('test event', 'test-event','2017-03-14', '2017-04-30');
+VALUES('Vefforritun 2', 'vefforritun-2','2022-01-1', '2022-01-1');
 
 INSERT INTO events (name,slug,created,updated )
-VALUES('test2 event', 'test2-event','2017-03-1', '2017-04-1');
+VALUES('Hugbúnaðarverkefni 2', 'hugbunadarverkefni-2','2022-01-1', '2022-01-1');
 
 INSERT INTO events (name,slug,created,updated )
-VALUES('test3 event', 'test3-event','2017-05-1', '2017-05-01');
+VALUES('Lokaverkefni 2022', 'lokaverkefni-20222','2022-01-1', '2022-01-01');
+
+
+INSERT INTO registration (name, comment, event, created)
+VALUES('Björgvin Júlíus Ásgeirsson', 'Þetta verður skemmtilegur áfangi', 1, '2022-01-01');
+
+INSERT INTO registration (name, comment, event, created)
+VALUES('Björgvin Júlíus Ásgeirsson', 'Þetta verður skemmtilegur áfangi', 2, '2022-01-01');
+
+INSERT INTO registration (name, comment, event, created)
+VALUES('Björgvin Júlíus Ásgeirsson', 'Þetta verður skemmtilegur áfangi', 3, '2022-01-01');
+
 
 INSERT INTO users (username, password) VALUES ('admin', '$2a$11$pgj3.zySyFOvIQEpD7W6Aund1Tw.BFarXxgLJxLbrzIv/4Nteisii');
